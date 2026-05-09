98ef36a5cc4e0661a214aa054d8afc6da6fb0ef6
Xiangcheng Zhang <zhangxiangcheng.ml@gmail.com>
2026-05-09 03:14:40 -0400

    update world model python env

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
-mamba activate wap_env
+mamba activate ei_world_model
 cd world_model
-python server.py 
+python server.py
 # python server.py --checkpoint-path data/ckpts/libero_object_ft/checkpoints/latest.ckpt
